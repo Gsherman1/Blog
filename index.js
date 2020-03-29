1a1422d2b44057e0a1d5f6565bd673ea92e97662
Gsherman1 <gsherman@hawaii.edu>
2020-03-29 11:53:13 +0000

    Added API button

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const https = require("https");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "This is the home page.";
+const aboutContent = "This is the home page.";
+const contactContent = "This is the home page.";
 
+const weatherContent = "This is the weather page.";
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -24,6 +26,12 @@ app.get("/", function(req, res){
     posts: posts
     });
 });
+
+app.get("/weather", function(req, res){
+  res.render("weather", {weatherContent: weatherContent});
+});
+
+
 
 app.get("/about", function(req, res){
   res.render("about", {aboutContent: aboutContent});
@@ -48,6 +56,43 @@ app.post("/compose", function(req, res){
   res.redirect("/");
 
 });
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html")
+});
+
+app.post("/", function(req, res) {
+    
+var city = String(req.body.cityInput);;
+console.log(req.body.cityInput);
+    
+const units = "imperial";
+const apiKey = "daff48f4973b01c5b2f0a2c732afafc2";
+const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=" + units + "&APPID=" + apiKey;
+    
+https.get(url, function(response){
+console.log(response.statusCode);
+        
+response.on("data", function(data){
+const weatherData = JSON.parse(data);
+const temp = weatherData.main.temp;
+const wind = weatherData.wind.speed;
+const humidity = weatherData.main.humidity;
+const city = weatherData.name;
+const weatherDescription = weatherData.weather[0].description;
+const icon = weatherData.weather[0].icon;
+const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+res.write("<h1> The weather is " + weatherDescription + "<h1>");
+res.write("<h2>The Wind Speed in " + city + " is " + wind + "mph" + "<h2>");
+res.write("<h2>The humidity is  " + humidity + "%" + "<h2>");
+res.write("<h2>The Temp is  " + temp + " Degrees F" + "<h2>");
+res.write("<img src=" + imageURL +">");
+res.send();
+});
+});
+})
+
 
 app.get("/posts/:postName", function(req, res){
   const requestedTitle = _.lowerCase(req.params.postName);
